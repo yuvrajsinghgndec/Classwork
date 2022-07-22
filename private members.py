a1cd2fa7d8d4f0b407c0dc697c9ebdaeab695e80
yuvrajsinghgndec <yuvrajkhera001@gmail.com>
2022-07-22 12:11:55 +0530

    second commit

--- private members.py
+++ private members.py
@@ -1,12 +1,12 @@
 class base:
     def __init__(self):
         self._a = "hello"
-       # self.__c = "hello"
+        self.__c = "hello"
 
 class derived(base):
     def __init__(self):
         base.__init__(self)
-       # print("calling private member", self.__c)
+        print("calling private member", self.__c)
 
 
 obj1 = base()
